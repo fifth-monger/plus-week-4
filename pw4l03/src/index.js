b222e914606898e1901e4f25365d4dedf495a3b1
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-09-17 18:42:29 -0500

    learning function parameters in JS

---
+++ pw4l03/src/index.js
@@ -0,0 +1,17 @@
+// Challenge
+// 1. Create a function isWindy
+// 2. Add a speed parameter to the function
+// 3. If speed is greater than 5, alert ‘It is windy’
+// 4. Else, alert ‘It is not windy’
+// 5. Call the function twice with different arguments to test both scenarios
+
+function isWindy(speed) {
+  if (speed > 5) {
+    alert("It is windy 🍃");
+  } else {
+    alert("It's not windy 👍");
+  }
+}
+let currentSpeed = 9;
+isWindy(currentSpeed);
+isWindy(3);
