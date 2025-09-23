b9e488d86a34a8798e87e11e50e308468e55f85f
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-09-23 17:10:43 -0500

    cleaner code!

--- pw4l07/src/index.js
+++ pw4l07/src/index.js
@@ -1,18 +1,17 @@
 // Challenge 1
 // Log li with id special
-console.log(document.querySelector("#special"));
+let specialCountry = document.querySelector("li#special");
+console.log(specialCountry);
 
 // Challenge 2
 // Log all li with class of country
-let countries = document.querySelectorAll("li");
+let countries = document.querySelectorAll("li.country");
 console.log(countries);
 
 // Challenge 3
 // Add class special to the li with id special
-let li = document.querySelector("#special");
-li.classList.add("special");
+specialCountry.classList.add("special");
 // Challenge 4
 // In your code, replace the content of the element with id special
 // by your favorite country (other than Portugal of course)
-let newCountry = document.querySelector("#special");
-newCountry.innerHTML = "Mexio 🇲🇽";
+specialCountry.innerHTML = "Mexico 🇲🇽";
