a6b854e54f921ff6e74b3578b1d008ea931b75d7
Vi <107654718+fifth-monger@users.noreply.github.com>
2025-09-24 15:28:19 -0500

    renamed somethings

--- pw4l08/src/index.js
+++ pw4l08/src/index.js
@@ -1,14 +1,16 @@
 // Challenge 1
 // Add click eventListener to the button. Alert `It is 18 degrees`
-function alertTemperature(event) {
+function alertTemperature() {
   alert("It is 18 degrees");
 }
 let button = document.querySelector("button");
 button.addEventListener("click", alertTemperature);
 // Challenge 2
 // When the button is clicked, replace button label by ‘18 degrees’
-function changeButton(event) {
-  secondClick.innerHTML = "18 degrees";
+
+function showTemperature() {
+  let buttonTwo = document.querySelector("button");
+  buttonTwo.innerHTML = "18 degrees";
 }
-let secondClick = document.querySelector("button");
-secondClick.addEventListener("click", changeButton);
+let showTemperatureButton = document.querySelector("button");
+showTemperatureButton.addEventListener("click", showTemperature);
